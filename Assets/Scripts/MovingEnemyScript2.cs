8ad22256e07c683c7b5baf52a04d1d7231351e52
みょんた <murayamaayaka@MMMacBook-Pro.local>
2017-05-15 18:48:06 +0900

    tst

---
+++ Assets/Scripts/MovingEnemyScript2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MovingEnemyScript2 : MonoBehaviour {
+
+
+	public Transform target;
+	UnityEngine.AI.NavMeshAgent agent; 
+
+
+	public GameObject enemy;
+	public GameObject PlayerC;
+	GameObject player; //プレイヤー
+
+	int enemyHP = 1;
+
+	float dis;
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.Find ("FPSControllers");
+		target = player.transform;
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> (); 
+		//player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 Apos = PlayerC.transform.position;
+		Vector3 Bpos = enemy.transform.position;
+		float dis = Vector3.Distance (Apos, Bpos);
+		//Debug.Log ("Distance : " + dis);
+		// 目的地をプレイヤーに設定する。
+		if (dis <= 7.0f) {
+			agent.SetDestination (target.position);
+		}
+	}
+
+	void Damage(){
+		enemyHP -= 1;
+		if(enemyHP == 0){
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player"){
+			col.gameObject.SendMessage ("PlayerDamage");
+			int PlayerHP = PlayerHPManager.Instance.playerHP;
+			PlayerHP--;
+			Debug.Log (GetComponent<Collider>().name);
+			Destroy(this.gameObject);			
+		}
+	}
+
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TestScript: MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadScene("06");
+	}
+		
+	}
+	
+	// Update is called once per frame
+	
+
