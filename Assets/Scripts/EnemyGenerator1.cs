87ffd257bf6dbba151133826e8c7002a20afa414
みょんた <murayamaayaka@MMMacBook-Pro.local>
2017-05-16 23:16:09 +0900

    外のフィールドのスポナー

--- Assets/Scripts/EnemyGenerator1.cs
+++ Assets/Scripts/EnemyGenerator1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyGenerator : MonoBehaviour {
+public class EnemyGenerator1 : MonoBehaviour {
 
 	public GameObject enemy;
 	//public GameObject Gene;   //時間があったら近づいてスポーン実装する
@@ -13,33 +13,33 @@ public class EnemyGenerator : MonoBehaviour {
 	UnityEngine.AI.NavMeshAgent agent; 
 
 	float dis;		
-			
+
 	//bool isCalled = false;		
 
 
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
-			//if (dis <= 10.0f) {		
-				InvokeRepeating ("Generate", 0, 30);		
-			}
+		//if (dis <= 10.0f) {		
+		InvokeRepeating ("Generate", 0, 5);		
+	}
 
 
 	void Update () {
 		//agent.SetDestination (target.position);
-
 	}
 
 	void Generate(){
-			Instantiate (enemy, transform.position, transform.rotation);
-		}
+		Instantiate (enemy, transform.position, transform.rotation);
+	}
 	/*
 	void OnTriggerEnter(Collider col){
 		if(col.gameObject.name == "FPSControllers"){
 			Debug.Log (GetComponent<Collider>().name);
-			InvokeRepeating ("Generate", 0, 5);
+			//Generate ();
+			//InvokeRepeating ("Generate", 0, 10);			
 		}
 	}
 	*/
-	}
+}
 
---
+++ Assets/Scripts/PlayerHPManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPManager : MonoBehaviour {
+
+	public static PlayerHPManager Instance =null;
+	public int playerHP = 10;
+
+	//public static float PlayerHP() {
+		//return playerHP;
+	//}
+	/*
+	void Awake(){
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad (this.gameObject);
+		} else {
+			Destroy (this);
+		}
+	}
+}
+*/
+}
