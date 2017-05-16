d4f689dc949afdaa565ae6c3fdfa79e84640ed9e
みょんた <murayamaayaka@MMMacBook-Pro.local>
2017-05-16 09:29:42 +0900

    残り

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour {
+
+	public readonly static Data Instance = new Data();
+
+	public int HP = 10;
+	public float time = 180.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
